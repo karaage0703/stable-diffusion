2939d5336f4ace706408e7dbee1d38d078852f6e
karaage0703 <coldchicken@gmail.com>
2022-10-18 23:36:57 +0900

    Add text to image function

--- scripts/ai_zoo_server.py
+++ scripts/ai_zoo_server.py
@@ -65,7 +65,189 @@ def load_img(path):
     return 2.*image - 1.
 
 
-def stable_diffusion():
+def text_to_image():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--outdir",
+        type=str,
+        nargs="?",
+        help="dir to write results to",
+        default="/output/"
+    )
+    parser.add_argument(
+        "--skip_save",
+        action='store_true',
+        help="do not save individual samples. For speed measurements.",
+    )
+    parser.add_argument(
+        "--ddim_steps",
+        type=int,
+        default=20,
+        help="number of ddim sampling steps",
+    )
+    parser.add_argument(
+        "--plms",
+        action='store_true',
+        help="use plms sampling",
+    )
+    parser.add_argument(
+        "--fixed_code",
+        action='store_true',
+        help="if enabled, uses the same starting code across samples ",
+    )
+    parser.add_argument(
+        "--ddim_eta",
+        type=float,
+        default=0.0,
+        help="ddim eta (eta=0.0 corresponds to deterministic sampling",
+    )
+    parser.add_argument(
+        "--n_iter",
+        type=int,
+        default=1,
+        help="sample this often",
+    )
+    parser.add_argument(
+        "--H",
+        type=int,
+        default=512,
+        help="image height, in pixel space",
+    )
+    parser.add_argument(
+        "--W",
+        type=int,
+        default=512,
+        help="image width, in pixel space",
+    )
+    parser.add_argument(
+        "--C",
+        type=int,
+        default=4,
+        help="latent channels",
+    )
+    parser.add_argument(
+        "--f",
+        type=int,
+        default=8,
+        help="downsampling factor",
+    )
+    parser.add_argument(
+        "--n_samples",
+        type=int,
+        default=1,
+        help="how many samples to produce for each given prompt. A.k.a. batch size",
+    )
+    parser.add_argument(
+        "--n_rows",
+        type=int,
+        default=0,
+        help="rows in the grid (default: n_samples)",
+    )
+    parser.add_argument(
+        "--scale",
+        type=float,
+        default=7.5,
+        help="unconditional guidance scale: eps = eps(x, empty) + scale * (eps(x, cond) - eps(x, empty))",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=-1,
+        help="the seed (for reproducible sampling)",
+    )
+    parser.add_argument(
+        "--precision",
+        type=str,
+        help="evaluate at this precision",
+        choices=["full", "autocast"],
+        default="autocast"
+    )
+
+
+    with open('/cache/input.json') as f:
+        input_json = json.load(f)
+
+    print(input_json['prompt'])
+    print(input_json['name'])
+    prompt = input_json['prompt']
+    animal_name = input_json['name']
+
+    opt = parser.parse_args()
+    random_walk = np.random.default_rng()
+    generator = torch.Generator(device="cuda")
+
+    if opt.seed < 0:
+        seed = generator.seed()
+    else:
+        seed = opt.seed
+    seed_everything(seed)
+
+    if opt.plms:
+        sampler = PLMSSampler(model)
+    else:
+        sampler = DDIMSampler(model)
+
+    os.makedirs(opt.outdir, exist_ok=True)
+    outpath = opt.outdir
+
+    batch_size = opt.n_samples
+    n_rows = opt.n_rows if opt.n_rows > 0 else batch_size
+    data = [batch_size * [prompt]]
+
+    sample_path = os.path.join(outpath, "ai_zoo")
+    os.makedirs(sample_path, exist_ok=True)
+    base_count = len(os.listdir(sample_path))
+
+    start_code = None
+    if opt.fixed_code:
+        start_code = torch.randn([opt.n_samples, opt.C, opt.H // opt.f, opt.W // opt.f], device=device)
+
+    precision_scope = autocast if opt.precision=="autocast" else nullcontext
+    with torch.no_grad():
+        with precision_scope("cuda"):
+            with model.ema_scope():
+                tic = time.time()
+                all_samples = list()
+                for n in trange(opt.n_iter, desc="Sampling"):
+                    for prompts in tqdm(data, desc="data"):
+                        uc = None
+                        if opt.scale != 1.0:
+                            uc = model.get_learned_conditioning(batch_size * [""])
+                        if isinstance(prompts, tuple):
+                            prompts = list(prompts)
+                        c = model.get_learned_conditioning(prompts)
+                        shape = [opt.C, opt.H // opt.f, opt.W // opt.f]
+                        samples_ddim, _ = sampler.sample(S=opt.ddim_steps,
+                                                         conditioning=c,
+                                                         batch_size=opt.n_samples,
+                                                         shape=shape,
+                                                         verbose=False,
+                                                         unconditional_guidance_scale=opt.scale,
+                                                         unconditional_conditioning=uc,
+                                                         eta=opt.ddim_eta,
+                                                         x_T=start_code)
+
+                        x_samples_ddim = model.decode_first_stage(samples_ddim)
+                        x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
+                        x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
+                        x_checked_image_torch = torch.from_numpy(x_samples_ddim).permute(0, 3, 1, 2)
+
+                        if not opt.skip_save:
+                            for x_sample in x_checked_image_torch:
+                                x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
+                                img = Image.fromarray(x_sample.astype(np.uint8))
+                                img.save(os.path.join(sample_path, f"{base_count:05}.png"))
+                                img.save(output_image_file_path)
+                                base_count += 1
+
+                toc = time.time()
+
+    print(f"Your samples are ready and waiting for you here: \n{outpath} \n"
+          f" \nEnjoy.")
+
+
+def image_to_image():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
@@ -228,7 +410,6 @@ def stable_diffusion():
         with precision_scope("cuda"):
             with model.ema_scope():
                 tic = time.time()
-                all_samples = list()
                 for n in trange(opt.n_iter, desc="Sampling"):
                     for prompts in tqdm(data, desc="data"):
                         uc = None
@@ -254,7 +435,6 @@ def stable_diffusion():
                                     if var_numb == 0: 
                                         Image.fromarray(x_sample.astype(np.uint8)).save(output_image_file_path)
                                     base_count += 1
-                            all_samples.append(x_samples)
 
                             for i in range(z_enc.size()[1]):
                                 for j in range(z_enc.size()[2]):
@@ -272,9 +452,6 @@ if __name__ == "__main__":
         s.bind(('0.0.0.0', PORT))
         s.listen()
 
-        print('Waiting for connection...')
-
-
         config = OmegaConf.load('configs/stable-diffusion/v1-inference.yaml')
         model = load_model_from_config(config, 'models/ldm/stable-diffusion-v1/model.ckpt')
 
@@ -286,6 +463,8 @@ if __name__ == "__main__":
         # print(result['text'])
         # prompt = result['text']
 
+        print('Waiting for connection...')
+
         while True:
             (connection, client) = s.accept()
             try:
@@ -294,9 +473,13 @@ if __name__ == "__main__":
                 print(data.upper().decode())
                 connection.send(data.upper())
 
-                if data.upper().decode() == 'START':
+                if data.upper().decode() == 'I2I':
                     print('start')
-                    stable_diffusion()
+                    image_to_image()
+
+                if data.upper().decode() == 'T2I':
+                    print('start')
+                    text_to_image()
 
                 if data.upper().decode() == 'CLEAR':
                     print('clear')
